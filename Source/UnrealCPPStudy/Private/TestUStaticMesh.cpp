64edf44b90f638d5317193d4a710b082ee285305
Fallshadow <www.sunshuchao@outlook.com>
2025-12-04 00:40:06 +0800

    ADD 尝试手动生成静态网格失败

---
+++ Source/UnrealCPPStudy/Private/TestUStaticMesh.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestUStaticMesh.h"
+
+// Sets default values
+ATestUStaticMesh::ATestUStaticMesh()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATestUStaticMesh::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATestUStaticMesh::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
